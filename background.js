e8b160b2c2c74cf1bde92f5a646ffc0515ed88a9
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-05-27 17:44:32 +0100

    Set background to listen for url and open new tab

--- background.js
+++ background.js
@@ -14,3 +14,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 // The above is called when a user clicks on the 'browser action'
 // onClicked it sends a message to the active tab
 // content.js now needs to listen for that message
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "open_new_tab") {
+            chrome.tabs.create({
+                "url": request.url
+            });
+        }
+    }
+);
+
+// The above is called when the backgoudn receives a message from the content script
