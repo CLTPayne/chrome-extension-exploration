4944c3b0ed49956d9a45d42c37f8e29048a69c50
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-05-27 17:36:15 +0100

    Send first url on page to background script
    * Enable useage with full chrome apis

--- content.js
+++ content.js
@@ -1,16 +1,31 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.message === 'clicked_browser_action') {
-            const firstHref = document.getElementsByTagName('a')[0].getAttribute("href"); 
-            console.log({firstHref})
-        }
-    }
-);
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//         if (request.message === 'clicked_browser_action') {
+//             const firstHref = document.getElementsByTagName('a')[0].getAttribute("href"); 
+//             console.log({firstHref})
+//         }
+//     }
+// );
 
 // The extraction of the href is now only run when the payload is received
 // in the form of a message from the background script.
 // Therefore everytime the user clicks the browser action button
 // the url should be logged to the console. 
 
-
 // alert('Hello, I am your explorative extension...')
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === 'clicked_browser_action') {
+            const firstHref = document.getElementsByTagName('a')[0].href;
+            console.log({firstHref});
+            chrome.runtime.sendMessage({
+                "message": "open_new_tab", 
+                "url": firstHref
+            });
+        }
+    }
+);
+
+// The above listener on the page, now also sends a return message to the backround
+// With the url to open in a new tab. 
