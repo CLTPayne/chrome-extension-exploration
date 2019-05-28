4ff5c0d0135bc1856403fc2451f8edc40c49e94d
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-05-28 22:20:24 +0100

    Add declariativeContent onPageChange rules

--- background-colour-extension/background.js
+++ background-colour-extension/background.js
@@ -1,10 +1,21 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-});
-
 // This adds a listener on installed and sets a value in the storage
 // Now multiple extension components will be able to access this value and update it
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+        console.log("The color is green.");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals : 'developer.chrome.com'}
+            })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
+
+
